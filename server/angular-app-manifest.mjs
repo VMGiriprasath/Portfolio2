8eb9c43da190eeffd27758d9c3388cf68b999a8f
girip <giriprasathvm2002@gmail.com>
2024-12-12 15:47:45 +0530

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -6,8 +6,8 @@ export default {
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 7654, hash: 'db13d9574972fdcff62ea99e5d09dca15a8b9390db91d4dece5b8db2de547d43', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 7981, hash: 'c1ec4db80ad930bbd1bf74d9be388eeb3e8b0f0f174dd1ad098667d286a839b5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 7660, hash: 'fa910038aaeed9e23c9a767aee3c46fd2b43a2f65222600d83b40f0444051ff7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 7987, hash: 'd2c85f80ccbf5862623d60f66c079911401efa8d062fa99936425d38a2ac4d5d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5XVWDKWT.css': {size: 105, hash: 'RdUNBt6AY6U', text: () => import('./assets-chunks/styles-5XVWDKWT_css.mjs').then(m => m.default)}
   },
 };
